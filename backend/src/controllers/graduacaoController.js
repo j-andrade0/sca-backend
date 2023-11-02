f012100b4a1d9ad0fc87cf2072b6bf9e9a9317c6
José Antonio de Andrade <andradejoseantonio0@gmail.com>
2023-11-02 19:18:40 -0300

    identacao

--- backend/src/controllers/graduacaoController.js
+++ backend/src/controllers/graduacaoController.js
@@ -1,90 +1,90 @@
 import Graduacao from '../models/Graduacao.js';
 
 class GraduacaoController {
-  static getAllEntities = async (req, res) => {
-    try {
-      const graduacoes = await Graduacao.findAll();
-      res.status(200).json(graduacoes);
-    } catch (error) {
-      res.status(500).send({ message: `${error.message}` });
-    }
-  };
+	static getAllEntities = async (req, res) => {
+		try {
+			const graduacoes = await Graduacao.findAll();
+			res.status(200).json(graduacoes);
+		} catch (error) {
+			res.status(500).send({ message: `${error.message}` });
+		}
+	};
 
-  static getEntityById = async (req, res) => {
-    try {
-      const entity = await Graduacao.findByPk(req.params.id);
-      if (entity) {
-        return res.status(200).json(entity);
-      } else {
-        return res.status(400).send({
-          message: `Id ${req.params.id} not found!`
-        });
-      }
-    } catch (error) {
-      return res.status(500).send({ message: `${error.message}` });
-    }
-  };
+	static getEntityById = async (req, res) => {
+		try {
+			const entity = await Graduacao.findByPk(req.params.id);
+			if (entity) {
+				return res.status(200).json(entity);
+			} else {
+				return res.status(400).send({
+					message: `Id ${req.params.id} não encontrado!`
+				});
+			}
+		} catch (error) {
+			return res.status(500).send({ message: `${error.message}` });
+		}
+	};
 
-  static createEntity = async (req, res) => {
-    try {
-      const { sigla, descricao, ordem } = req.body;
+	static createEntity = async (req, res) => {
+		try {
+			const { sigla, descricao, ordem } = req.body;
 
-      const createdEntity = await Graduacao.create({
-        sigla,
-        descricao,
-        ordem
-      });
-      res.status(201).json(createdEntity);
-    } catch (error) {
-      if (error.name == 'SequelizeUniqueConstraintError') {
-        res.status(400).send({ message: 'Valores já cadastrados!' });
-      } else {
-        res.status(500).send({ message: `${error.message}` });
-      }
-    }
-  };
+			const createdEntity = await Graduacao.create({
+				sigla,
+				descricao,
+				ordem
+			});
+			res.status(201).json(createdEntity);
+		} catch (error) {
+			if (error.name == 'SequelizeUniqueConstraintError') {
+				res.status(400).send({ message: 'Valores já cadastrados!' });
+			} else {
+				res.status(500).send({ message: `${error.message}` });
+			}
+		}
+	};
 
-  static updateEntity = async (req, res) => {
-    try {
-      const { sigla, descricao, ordem } = req.body;
-      const entityId = req.params.id;
+	static updateEntity = async (req, res) => {
+		try {
+			const { sigla, descricao, ordem } = req.body;
+			const entityId = req.params.id;
 
-      const [updatedRows] = await Graduacao.update(
-        {
-          sigla,
-          descricao,
-          ordem
-        },
-        { where: { id: entityId } }
-      );
+			const [updatedRows] = await Graduacao.update(
+				{
+					sigla,
+					descricao,
+					ordem
+				},
+				{ where: { id: entityId } }
+			);
 
-      if (updatedRows > 0) {
-        res.status(200).send({ message: 'Entity updated successfully' });
-      } else {
-        res.status(400).send({
-          message: `Id ${entityId} not found!`
-        });
-      }
-    } catch (error) {
-      res.status(500).send({ message: `${error.message}` });
-    }
-  };
+			if (updatedRows > 0) {
+				res.status(200).send({ message: 'Entidade atualizada com sucesso!' });
+			} else {
+				res.status(400).send({
+					message: `Id ${entityId} não encontrado!`
+				});
+			}
+		} catch (error) {
+			res.status(500).send({ message: `${error.message}` });
+		}
+	};
 
-  static deleteEntity = async (req, res) => {
-    try {
-      const entity = await Graduacao.findByPk(req.params.id);
-      if (entity) {
-        await entity.destroy();
-        return res.status(204).send();
-      } else {
-        return res.status(400).send({
-          message: `Id ${req.params.id} not found!`
-        });
-      }
-    } catch (error) {
-      return res.status(500).send({ message: `${error.message}` });
-    }
-  };
+	static deleteEntity = async (req, res) => {
+		try {
+			const entity = await Graduacao.findByPk(req.params.id);
+			if (entity) {
+				await entity.destroy();
+				return res.status(204).send();
+			} else {
+				return res.status(400).send({
+					message: `Id ${req.params.id} não encontrado!`
+				});
+			}
+		} catch (error) {
+			return res.status(500).send({ message: `${error.message}` });
+		}
+	};
 }
 
 export default GraduacaoController;
