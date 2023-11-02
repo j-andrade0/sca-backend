4911368fc823f559f2ce649fed03a82d60bbb2dc
José Antonio de Andrade <andradejoseantonio0@gmail.com>
2023-11-02 14:34:43 -0300

    removed restart of database

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ import Visitante from './models/Visitante.js';
 import RegistroAcesso from './models/Registro_Acesso.js';
 
 try {
-	await db.sync({ force: true }); // await db.sync({ force: true }); to reset database everytime, and await db.sync(); to keep the records
+	await db.sync(); // await db.sync({ force: true }); to reset database everytime, and await db.sync(); to keep the records
 	console.warn('All models were synchronized successfully.');
 } catch (error) {
 	console.error(error);
