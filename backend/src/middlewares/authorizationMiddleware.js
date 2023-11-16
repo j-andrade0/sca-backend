fc421d9a781e7766970616cb5a9b074de5b8d84d
José Antonio de Andrade <andradejoseantonio0@gmail.com>
2023-11-16 15:50:01 -0300

    remove trigger from middleware

--- backend/src/middlewares/authorizationMiddleware.js
+++ backend/src/middlewares/authorizationMiddleware.js
@@ -1,13 +1,9 @@
-function verifyAuthorization({nivel_acesso}) {
+function verifyAuthorization({ nivel_acesso }) {
 	return (req, res, next) => {
-		if (true) { // trigger to desativate the middleware, must be removed
-			if (req.header('access-level') >= nivel_acesso) {
-				next();
-			} else {
-				return res.status(401).json({ message: 'Baixo nível de acesso!' });
-			}
-		} else {
+		if (req.header('access-level') >= nivel_acesso) {
 			next();
+		} else {
+			return res.status(401).json({ message: 'Baixo nível de acesso!' });
 		}
 	};
 }
