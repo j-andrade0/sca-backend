806418fda4b69cbeba320e53bb3927308b126227
José Antonio de Andrade <andradejoseantonio0@gmail.com>
2023-11-17 14:33:03 -0300

    cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
+import corsOptions from './config/corsConfig.js';
+import cors from 'cors'
 
 try {
 	await db.sync(); // await db.sync({ force: true }); to reset database everytime, and await db.sync(); to keep the records
@@ -10,7 +12,10 @@ try {
 }
 
 const app = express();
+
+app.use(cors(corsOptions))
 app.use(express.json());
+
 routes(app);
 
 export default app;
